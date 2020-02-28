8891d077260d86b07ad2d0c224c5e383942bb51b
github username <github email address>
2020-02-28 13:31:31 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+const map = (array, fun) => {
+    return array.map(number => fun(number))
+} 
+
+// const reduce = (array, fun, starting = 0) => {
+//     const start = starting
+//     return array.reduce((num, start) => fun(num, start))
+// }
+
+function reduce(src, cb, starting){
+    let r = (!!starting) ? starting : src[0]
+    let i = (!!starting) ? 0 : 1
+  
+    for (; i < src.length; i++) {
+      r = cb(src[i], r)
+    }
+  
+    return r;
+}
